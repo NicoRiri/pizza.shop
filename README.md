5001101f4181d0997cb23556747dd09f07655183
Nicolas Bernardet <57874200+NicoRiri@users.noreply.github.com>
2023-11-07 18:39:55 +0100

    Merge TD5 (#1)
    * docs: maj du readme
    
    * refactor: l'espace me stressait
    
    * feat: ajout basic auth depuis pizza shop
    
    * fix: oublie des arguments de routes
    
    * fix: import inutile
    
    * fix: mauvaise route pour guzzle
    
    * fix: mauvais message d'erreur
    
    * fix: mauvais arguments dans middleware
    
    * fix: changement complet du middleware
    
    * feat: ajoue de la vérification sur toutes les routes sauf connexion
    
    * composer: ajoue de guzzle

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# pizza.shop
+# Pizza Shop
+Pizza Shop est un projet de qu'on a fait en 3ème année de BUT informatique pour apprendre à construire des API ainsi que manier les redrections à d'autres API pour gérer des tâches externes tels que l'authentification JWT.
+
+## ️👥 Auteurs
+- Bernardet Nicolas
+- Gallion Laura
+- Demarque Amaury
+- Oudin Clément
+
+## 💯Déploiement
+Pour déployer l'application il suffit de lancer les conteneurs de pizza.shop.components
+``cd pizza.shop.components``
+``docker compose create``
+``docker compose start``
--- shop.pizza-shop/config/routes.php
+++ shop.pizza-shop/config/routes.php
@@ -7,10 +7,14 @@ use Psr\Http\Message\ResponseInterface as Response;
 return function( \Slim\App $app):void {
 
     $app->post('/commandes[/]', \pizzashop\shop\app\actions\CreerCommandeAction::class)
+        ->add(\pizzashop\shop\app\middleware\midCheckToken::class)
         ->setName('creer_commande');
-
     $app->get('/commandes/{id}[/]', \pizzashop\shop\app\actions\AccederCommandeAction::class)
+        ->add(\pizzashop\shop\app\middleware\midCheckToken::class)
         ->setName('accederCommande');
     $app->patch("/commandes/{id}[/]", \pizzashop\shop\app\actions\ValiderCommandeAction::class)
+        ->add(\pizzashop\shop\app\middleware\midCheckToken::class)
         ->setName('validerCommande');
+    $app->post("/connexion[/]", \pizzashop\shop\app\actions\SignInAction::class)
+        ->setName("connexion");
 };
